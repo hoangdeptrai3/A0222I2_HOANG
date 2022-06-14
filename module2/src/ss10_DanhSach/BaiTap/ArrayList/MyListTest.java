0be9d241ee6eb7fb3c299cba48441e80a1710a5e
hoangdeptrai3 <thaiquanghuyhoang10a7@gmail.com>
2022-06-14 23:14:31 +0700

    up

--- module2/src/ss10_DanhSach/BaiTap/ArrayList/MyListTest.java
+++ module2/src/ss10_DanhSach/BaiTap/ArrayList/MyListTest.java
@@ -1,4 +1,30 @@
 package ss10_DanhSach.BaiTap.ArrayList;
 
 public class MyListTest {
+    public static void main(String[] args) {
+        MyList<Integer> arrayList = new  MyList<>();
+        arrayList.add(1);
+        // qg
+        arrayList.remove(0);
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(6);
+        arrayList.add(2, 23);
+
+        System.out.println(arrayList.contains(6));
+        System.out.println(arrayList.indexOf(3));
+        System.out.println(arrayList.size());
+//    arrayList.clear();
+
+        try {
+            MyList<Integer> arrayClone = ( MyList<Integer>) arrayList.clone();
+            for (int i = 0; i < arrayClone.size(); i++) {
+                System.out.println(arrayClone.get(i));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+
