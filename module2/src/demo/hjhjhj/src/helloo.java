28642e2875edd4ee19b29e52664bf56d55842c58
hoangdeptrai3 <99674304+hoangdeptrai3@users.noreply.github.com>
2022-12-24 21:53:59 +0700

    up

---
+++ module2/src/demo/hjhjhj/src/helloo.java
@@ -0,0 +1,9 @@
+public class helloo {
+    static int sum(int a,int b){
+        int sum = a + b;
+        return sum;
+    }
+    public static void main(String[] args) {
+        sum(10,20);
+    }
+}
